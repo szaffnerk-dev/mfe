df98be4e9b37c6cd49b83b2460b93ccd07841d17
Kamil Szaffner <szaffnerk@gmail.com>
2026-02-11 18:22:09 +0100

    fix on container

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -3,7 +3,6 @@ import { mount } from "marketing/MarketingApp";
 
 export default function MarketingApp() {
   const ref = useRef(null);
-  console.log(mount);
 
   useEffect(() => {
     mount(ref.current);
