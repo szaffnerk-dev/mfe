1e126f4bde93e131c4a4c940d1ea57bd74595ecc
Kamil Szaffner <szaffnerk@gmail.com>
2026-02-11 17:48:15 +0100

    add first workflow

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -1,6 +1,5 @@
 const { merge } = require("webpack-merge");
 
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
@@ -21,9 +20,6 @@ const devConfig = {
         "./MarketingApp": "./src/bootstrap.js",
       },
       shared: packageJson.dependencies,
-    }),
-    new HtmlWebpackPlugin({
-      template: "public/index.html",
     }),
   ],
 };
